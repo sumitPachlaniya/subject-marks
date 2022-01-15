c43be40750ce62cb986a5144903a13551440e3c5
sumitPachlaniya <97600179+sumitPachlaniya@users.noreply.github.com>
2022-01-15 21:30:46 +0530

    Add files via upload

---
+++ playground (10).swift
@@ -0,0 +1,19 @@
+var marks = 90
+
+switch marks {
+
+case 88:
+    print("pass with 88 in hindi")
+case 89:
+    print("pass with 89 in english")
+case 60:
+    print("pass with 60 in physics")
+case 90:
+    print("pass with 90 in mathematics")
+case 75:
+    print("pass with 75 chemistrye")
+    
+default:
+    print("he/she didn't get such marks in any subject") 
+
+}
